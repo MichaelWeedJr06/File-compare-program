632f620ce37e197ec2b631591d6f62d387d4705a
Michael Weed <michaelweedjr@gmail.com>
2026-03-26 13:17:51 -0400

    added improvement.

--- scrapers/scrapLinks.js
+++ scrapers/scrapLinks.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 async function scrapeLinks(url) {
-  const search = "blob";
+  const search = ["blob","tree"];
   try {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
     const links = new Set();
     $("a").each((_, element) => {
       const href = $(element).attr("href");
-      if (href.includes(search)) links.add(href);
+      if (href.includes(search[0] || search[1])) links.add(href);
     });
     return links;
   } catch (error) {
